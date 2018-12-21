eb698e9995e59ca0a8aa9398258be7e175dd9cd7
bondor.janos <bondor.janos@virgo.hu>
2018-12-21 11:06:49 +0100

    IU-2018.3.2 <morfie@virgo286 Create baseRefactoring.xml, markdown.xml, PHP Interface Doc Comment.php, PHP Class.php, ide.general.xml, _@user_Default.icls, project.default.xml, diff.xml, PHP Trait Doc Comment.php, Default copy.xml, ea_extended.xml, PHP Constructor.php, go.dep.xml, PHP Class Doc Comment.php, Default.xml, keymap.xml, find.xml, ui.lnf.xml, filetypes.xml, php.xml, remote-servers.xml, colors.scheme.xml, notifications.xml, git.xml, PHP Trait.php, databaseDrivers.xml, PHP Template File༏ext༏.html.php, PHP Getter Method.php, PHP Function Doc Comment.php, goLibraries.xml, Default.xml, PHP.xml, go.sdk.xml, PHP Fluent Setter Method.php, jdk.table.xml, laf.xml, PHP File Header.php, web-browsers.xml, vcs.xml, editor.codeinsight.xml, PHP Setter Method.php, PHP Interface.php, databaseSettings.xml, editor.xml, usageView.xml, debugger.xml, path.macros.xml, updates.xml

---
+++ fileTemplates/PHP Template File༏ext༏.html.php
@@ -0,0 +1,5 @@
+<?php
+#parse("PHP File Header.php")
+
+/* @var \Symfony\Bundle\FrameworkBundle\Templating\PhpEngine \$view */
+/* @var \Symfony\Bundle\FrameworkBundle\Templating\PhpEngine \$this */
